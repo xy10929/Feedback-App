9a749eb7d13e2f17502ec5b3fafd6e6eb6fcfabf
Zhengyu Yang <yangzy0105@gmail.com>
2023-07-04 19:34:31 -0700

    7.4

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,17 +1,17 @@
-
+import Card from "./shared/Card"
 
 function FeedbackItem({ item }) {
 
-
   return (
-    <div className='card'>
+    <Card>
       <div className="num-display">{item.rating}</div>
       <div className="text-display">
         {item.text}
       </div>
 
-    </div>
+    </Card>
   )
 }
+
 
 export default FeedbackItem
---
+++ src/components/shared/Card.jsx
@@ -0,0 +1,17 @@
+function Card({ children, reverse }) {
+  return (
+    <div className="card" style={{
+      backgroundColor: reverse ? 'rgba(0,0,0,0.4)' : '#ffff',
+      color: reverse ? '#fff' : '#000'
+    }}>
+      {children}
+    </div>
+  )
+}
+
+
+Card.defaultProps = {
+  reverse: false
+}
+
+export default Card
